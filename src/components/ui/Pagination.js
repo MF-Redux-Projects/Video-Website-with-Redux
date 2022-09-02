0a24d8340afc874aeb35ca29e623f685f504dbb3
mehedifoysal <mehedifoysal8@gmail.com>
2022-09-02 21:04:24 +0600

    pagination slice create

--- src/components/ui/Pagination.js
+++ src/components/ui/Pagination.js
@@ -1,19 +1,29 @@
+import {useSelector} from "react-redux";
+
 export default function Pagination() {
+    const {videos} = useSelector(state => state.videos);
+    const {page, limit} = useSelector(state => state.pagination);
+
+    const totalPage = Math.ceil(videos.length / limit);
+    const pages = Array.from({length: totalPage}, (_, i) => i + 1);
+
     return (
         <section className="pt-12">
             <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 justify-end">
+                {
+                    pages.map(page => (
+                        <div className="bg-blue-600 text-white px-4 py-1 rounded-full" onClick={}>
+                            {page}
+                        </div>
+                    ))
+                }
                 <div className="bg-blue-600 text-white px-4 py-1 rounded-full">
                     1
                 </div>
-                <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full">
+                {/*<div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full">
                     2
-                </div>
-                <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full">
-                    3
-                </div>
-                <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full">
-                    4
-                </div>
+                </div>*/}
+
             </div>
         </section>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,4 @@
-import Footer from "../components/Footer";
 import VideoGrid from "../components/grid/VideoGrid";
-import Navbar from "../components/navbar/Navbar";
 import Tags from "../components/tags/Tags";
 import Pagination from "../components/ui/Pagination";
 
