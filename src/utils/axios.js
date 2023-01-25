96291b27226775e6662328008cbbafa0eed48d17
mehedifoysal <mehedifoysal8@gmail.com>
2023-01-26 01:12:04 +0600

    local server dlt

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://lws-fake-product-api.herokuapp.com',
+    baseURL: 'https://mf-redux.onrender.com',
 });
 
 export default axiosInstance;
